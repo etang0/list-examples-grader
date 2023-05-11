4df1c9b78672c3fe5395510f9d5a91b0acf8c069
etang0 <44424518+etang0@users.noreply.github.com>
2023-05-11 13:51:00 -0700

    lab6 1.0

--- grade.sh
+++ grade.sh
@@ -6,7 +6,39 @@ rm -rf grading-area
 mkdir grading-area
 
 git clone $1 student-submission
+
+FILEPATH=$(find "student-submission" -name "*.java")
+if ! [[ -e $FILEPATH ]]
+    then
+        echo "Submission does not exist"
+        exit
+fi
+
+if ! [[ -f $FILEPATH ]]
+    then
+        echo "Submission is not a file"
+        exit
+fi
+
+mv $FILEPATH grading-area
+
 echo 'Finished cloning'
+
+FILEPATH2=$(find "grading-area" -name "*.java")
+echo $FILEPATH2
+
+javac $FILEPATH2
+if [[ $? ]] 
+then echo $FILEPATH2 compiled
+else echo $FILEPATH2 failed to compile
+fi
+
+javac -cp .:libs/junit-4.13.2.jar:libs/hamcrest-core-1.3.jar
+if [[ $? ]] 
+then echo junit compiled
+else echo junit failed to compile
+fi
+
 
 
 # Draw a picture/take notes on the directory structure that's set up after
