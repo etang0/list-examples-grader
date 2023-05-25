18e735498e3760ad1905f7447f64b5389e763603
etang0 <44424518+etang0@users.noreply.github.com>
2023-05-25 13:38:44 -0700

    grading script 1.0

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.:grading-area:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -25,7 +25,6 @@ mv $FILEPATH grading-area
 echo 'Finished cloning'
 
 FILEPATH2=$(find "grading-area" -name "*.java")
-echo $FILEPATH2
 
 javac $FILEPATH2
 if [[ $? ]] 
@@ -33,13 +32,10 @@ then echo $FILEPATH2 compiled
 else echo $FILEPATH2 failed to compile
 fi
 
-javac -cp .:libs/junit-4.13.2.jar:libs/hamcrest-core-1.3.jar
-if [[ $? ]] 
-then echo junit compiled
-else echo junit failed to compile
-fi
-
-
+cp TestListExamples.java grading-area
+javac -cp $CPATH grading-area/ListExamples.java grading-area/TestListExamples.java
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples | grep -E "There was|) test" > feedback.txt
+cat feedback.txt
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
